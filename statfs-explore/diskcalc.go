f5039358f24bf0c54454e35768567ea63c983ca7
Borats Nolan <borats@i.dont.use.email>
2018-01-27 13:52:29 +1100

    add in disk calc code exploration.

---
+++ statfs-explore/diskcalc.go
@@ -0,0 +1,53 @@
+// +build linux darwin openbsd
+// +build amd64
+
+package main
+
+import (
+	"syscall"
+)
+
+var (
+	unit           float64
+	unitStr        string
+	totalSizeBytes float64
+	statfs         syscall.Statfs_t
+)
+
+const (
+	TByte = 1024.0 * 1024.0 * 1024 * 1024
+	GByte = TByte / 1024.0
+	MByte = GByte / 1024.0
+	KByte = MByte / 1024.0
+)
+
+type diskSize struct {
+	total     float64
+	available float64
+	statfs    syscall.Statfs_t
+}
+
+func diskCalc() (*diskSize, error) {
+	if err := syscall.Statfs(*path, &statfs); err != nil {
+		return nil, err
+	}
+	totalSizeBytes = float64(statfs.Blocks) * float64(statfs.Bsize)
+	if totalSizeBytes >= KByte && totalSizeBytes <= MByte {
+		unit = KByte
+		unitStr = "KB"
+	} else if totalSizeBytes >= MByte && totalSizeBytes <= GByte {
+		unit = MByte
+		unitStr = "MB"
+	} else if totalSizeBytes >= GByte && totalSizeBytes <= TByte {
+		unit = GByte
+		unitStr = "GB"
+	} else if totalSizeBytes >= TByte {
+		unit = TByte
+		unitStr = "TB"
+	}
+	return &diskSize{
+		total:     totalSizeBytes / unit,
+		available: (float64(statfs.Bavail) * float64(statfs.Bsize)) / unit,
+		statfs:    statfs,
+	}, nil
+}
---
+++ statfs-explore/main.go
@@ -0,0 +1,35 @@
+// +build linux darwin openbsd
+// +build amd64
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/davecgh/go-spew/spew"
+)
+
+var (
+	path  = flag.String("path", "/", "path to calculate the usage details on")
+	debug = flag.Bool("debug", false, "print statfs values")
+)
+
+func init() {
+	flag.Parse()
+}
+
+func main() {
+	diskSize, err := diskCalc()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if *debug == true {
+		spew.Dump(diskSize)
+		spew.Dump(diskSize)
+	}
+	fmt.Printf("total space %s = %.2f %s\n", *path, diskSize.total, unitStr)
+	fmt.Printf("available space %s = %.2f %s\n", *path, diskSize.available, unitStr)
+}
