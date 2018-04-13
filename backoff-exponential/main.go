bde62b3dd18c0d35633525b551c38e5e6fd13299
Borats Nolan <borats@i.dont.use.email>
2018-04-13 22:59:42 +1000

    explore exponential backoff in go

---
+++ backoff-exponential/main.go
@@ -0,0 +1,37 @@
+package main
+
+// Explore backoff exponentially
+import (
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/cenkalti/backoff"
+)
+
+var (
+	msg = "hello world!\n"
+)
+
+func main() {
+	b := backoff.NewExponentialBackOff()
+	b.MaxElapsedTime = 0 // indefinitely
+	for {
+		s, err := net.Dial("tcp", "dns1.local:2222")
+		if err != nil {
+			t := b.NextBackOff()
+			fmt.Printf("err %s occurred. connecting again in %+v and closing socket\n", err, t)
+			if s != nil {
+				s.Close()
+			}
+			time.Sleep(t)
+		} else {
+			n, err := s.Write([]byte(msg))
+			if err != nil {
+				return
+			}
+			fmt.Printf("connected and wrote %d bytes\n", n)
+			s.Close() // we are done
+		}
+	}
+}
